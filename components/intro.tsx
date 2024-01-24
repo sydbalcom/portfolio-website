fc0f3b3cae87f6c79a5399ff2dac38649fd33df0
sydbalcom <sbalcom3@gatech.edu>
2024-01-24 16:12:23 -0500

    Add intro subheader

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,4 +1,3 @@
-import { CMS_NAME } from '../lib/constants'
 import Image from 'next/image'
 import FlowerLogo from './flower-logo';
 
@@ -16,7 +15,7 @@ const Intro = () => {
       </div>
       <div className="mb-12 mx-12 pt-16 flex flex-col items-center">
         <div className="w-fit">
-          <p className="text-4xl mb-4 md:mb-5 font-openSauceBold font-style: italic">Hey, I'm</p>
+          <p className="text-4xl mb-1 md:mb-2 font-openSauceBold font-style: italic">Hey, I'm</p>
           <h1 className="text-7xl font-cosmicaBold tracking-tighter leading-tight md:pr-8">
             <span className="flex inline-flex gap-x-6 mr-6">
               SYD
@@ -27,6 +26,7 @@ const Intro = () => {
             </span>
             <span>BALCOM</span>
           </h1>
+          <p className="w-fit text-xl md:text-2xl mt-5 md:mt-4 px-4 py-2 rounded-md font-openSauceMed bg-[#EFEFEF]">Front-End Developer & UI/UX Designer</p>
         </div>
       </div>
     </section>
