9341065546f724352eded7cc8b5f0d70b3bb5452
sydbalcom <sbalcom3@gatech.edu>
2024-01-30 10:06:37 -0500

    Reformat nav bar

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -10,17 +10,17 @@ const NavBar = ({ navItems }: Props) => {
     // const [selectedIndex, setSelectedIndex] = useState(-1);
     return (
         <nav className="px-12 py-6 gap-9 w-full bg-[#F9F9F9] flex flex-row items-center
-        sticky top-0 text-md font-cosmicaBold drop-shadow-sm shadow-[#295092]">
-            <Link href="/" className="opacity-85 hover:opacity-100 hover:scale-110 hover:box-shadow-sm">
+        sticky top-0 text-md font-cosmicaBold drop-shadow-md shadow-[#295092] rounded-b-xl">
+            <Link href="/" className="opacity-90 hover:opacity-100 hover:scale-110 hover:box-shadow-sm">
                 <SBLogo />
             </Link>
-            <Link href="/#About" className="opacity-85 hover:opacity-100 hover:scale-110">
+            <Link href="/#About" className="opacity-90 hover:opacity-100 hover:scale-110">
                 <h3>ABOUT</h3>
             </Link>
-            <Link href="/#Work" className="opacity-85 hover:opacity-100 hover:scale-110">
+            <Link href="/#Work" className="opacity-90 hover:opacity-100 hover:scale-110">
                 <h3>WORK</h3>
             </Link>
-            <Link href="/#Play" className="opacity-85 hover:opacity-100 hover:scale-110">
+            <Link href="/#Play" className="opacity-90 hover:opacity-100 hover:scale-110">
             <h3>PLAY</h3>
             </Link>
         </nav>
--- components/site-sections.tsx
+++ components/site-sections.tsx
@@ -6,8 +6,9 @@ type Props = {
 
 const SiteSection = ({name}: Props) => {
     return (
-        <section id={name} className="mx-12 pb-12 flex flex-row items-center gap-2">
-            <h2 className="text-2xl font-openSauceBold">{name} make bg flowers scroll slower than other elements; bring back diff colors</h2>
+        <section id={name} className="mx-12 pt-28 pb-6 flex flex-row items-center gap-2">
+            <h2 className="text-2xl font-openSauceBold">{name}</h2>
+            {/* TODO: make bg flowers scroll slower than other elements; bring back diff colors */}
             <FlowerLogo
                 fillColor='#295092'
                 width='24'
